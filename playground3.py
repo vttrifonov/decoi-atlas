8600fc02b4ffbd7f98229b80769c278740bde95d
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-10-12 00:28:55 +0000

    playground

--- playground3.py
+++ playground3.py
@@ -4,49 +4,89 @@ import pandas as pd
 import xarray as xa
 from plotnine import *
 import matplotlib.pyplot as plt
-
-
-# %%
-__package__ = 'decoi_atlas'
-import rpy2.robjects as ro
-from rpy2.robjects import r as R
-from rpy2.robjects import pandas2ri, numpy2ri
-from rpy2.robjects.conversion import localconverter
-import sparse
-from ._helpers import config
-
-R(f'source("{config.root}/_data.R")')
+from decoi_atlas._data import data
+import decoi_atlas.common.helpers
 
 # %%
-x1 = R('data$c2_wb_pbmc@meta.data')
-x1 = pandas2ri.rpy2py(x1)
-x1.index.names = ['cell_id']
-x1 = x1.to_xarray()
-x1 = x1.rename({
-    k: 'cell_'+k for k in x1.keys()
-})
+x = data.c2_wb_pbmc
 
-x3 = R('data$c2_wb_pbmc[["RNA"]]@meta.features')
-x3 = pandas2ri.rpy2py(x3)
-x3.index.names = ['feature_id']
-x3 = x3.to_xarray()
-x3 = x3.rename({
-    k: 'feature_'+k for k in x3.keys()
-})
+# %%
+x['rpk'] = x.counts.copy()
+x.rpk.data = x.rpk.data.asformat('coo')
+x['total'] = x.rpk.sum(dim='feature_id').todense()
+x['rpk'] = 1e4*x.rpk/x.total
 
-x2 = R((
-    'x <- GetAssayData(data$c2_wb_pbmc[["RNA"]], slot="counts");'
-    'list(x@i, x@p, x@x, rownames(x), colnames(x))'
-))
-x2 = [numpy2ri.rpy2py(x) for _, x in x2.items()]
-x2 = xa.DataArray(
-    sparse.GCXS((x2[2], x2[0], x2[1]), shape=(len(x2[3]), len(x2[4]))),
-    [('feature_id', x2[3]), ('cell_id', x2[4])],
-    name='counts'
+# %%
+import statsmodels.api as sm
+x2 = x.sel(feature_id=['C5AR1', 'C5AR2'])
+x2 = x2.sel(cell_id=x2.cell_diagnosis!='')
+x2 = x2.sel(cell_id=x2.cell_cells=='PBMC')
+x2 = x2.sel(cell_id=~x2['cell_blueprint.labels'].isin(['Macrophages', 'Erythrocytes']))
+x2 = x2.todense()
+x2 = xa.merge([
+    x2.counts.assign_coords(
+        feature_id=['counts_'+k for k in x2.feature_id.data]
+    ).to_dataset(dim='feature_id'),
+    x2.rpk.assign_coords(
+        feature_id=['rpk_'+k for k in x2.feature_id.data]
+    ).to_dataset(dim='feature_id'),
+    x2.drop_dims('feature_id')
+])
+x2 = x2.to_dataframe()
+x2['C5AR1>0'] = np.where(x2.counts_C5AR1>0, 'C5AR1>0', 'C5AR1=0')
+x2['C5AR2>0'] = np.where(x2.counts_C5AR2>0, 'C5AR2>0', 'C5AR2=0')
+x2['C5AR>0'] = x2['C5AR1>0']+','+x2['C5AR2>0']
+
+def plot_table(x3):
+    x1 = [x3.table_orig.index.name, x3.table_orig.columns.name]
+    x3 = pd.concat([
+        x3.table_orig.reset_index().\
+            melt(id_vars=x1[:1]).\
+            rename(columns={'value': 'table'}).\
+            set_index(x1),
+        x3.resid_pearson.reset_index().\
+            melt(id_vars=x1[:1]).\
+            rename(columns={'value': 'resid'}).\
+            set_index(x1),
+        x3.fittedvalues.reset_index().\
+            melt(id_vars=x1[:1]).\
+            rename(columns={'value': 'fit'}).\
+            set_index(x1)
+    ], axis=1).reset_index()
+    x3['delta'] = x3['table'].astype(str) + '\n' + x3['fit'].astype(int).astype(str)
+
+    return (
+        ggplot(x3)+
+            aes(x1[1], x1[0])+
+            geom_tile(aes(fill='resid'))+
+            geom_text(aes(label='delta'))+
+            scale_fill_gradient2(
+                low='blue', mid='white', high='red',
+                midpoint=0
+            )
+    )
+
+# %%
+x3 = sm.stats.Table.from_data(x2[['cell_blueprint.labels', 'cell_diagnosis']])
+print(
+    plot_table(x3)
+)
+
+x3 = sm.stats.Table.from_data(x2[['cell_blueprint.labels', 'C5AR1>0']])
+print(
+    plot_table(x3)+labs(x='')
+)
+
+x3 = sm.stats.Table.from_data(x2[['cell_diagnosis', 'C5AR1>0']])
+print(
+    plot_table(x3)+labs(x='')
+)
+
+x3 = x2[x2['cell_blueprint.labels']=='Neutrophils']
+x3 = sm.stats.Table.from_data(x3[['cell_diagnosis', 'C5AR1>0']])
+print(
+    plot_table(x3)+labs(x='', title='Neutrophils')
 )
 
 
-x4 = xa.merge([
-    x2.to_dataset(), x1, x3
-], join='inner')
 # %%
