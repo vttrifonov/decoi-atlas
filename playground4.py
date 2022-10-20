9c29cc55e1e34094fc6cafff3385d5848593f64e
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-10-20 20:38:32 +0000

    playground

--- playground4.py
+++ playground4.py
@@ -34,16 +34,54 @@ def anova(A1, A2, B):
     return X, r2, p
 
 # %%
-x = data.c2_neutrophils_integrated
-x['rpk'] = x.counts.copy()
-x.rpk.data = x.rpk.data.asformat('coo')
-x['total'] = x.rpk.sum(dim='feature_id').todense()
-x['rpk'] = 1e4*x.rpk/x.total
+class _analysis:
+    @compose(property, lazy)
+    def data(self):
+        x = data.c2_neutrophils_integrated
+        x['rpk'] = x.counts.copy()
+        x.rpk.data = x.rpk.data.asformat('coo')
+        x['total'] = x.rpk.sum(dim='feature_id').todense()
+        x['rpk'] = 1e4*x.rpk/x.total
+        return x
+
+    @compose(property, lazy)
+    def data1(self):
+        x = self.data
+        x2 = x.sel(feature_id=['C5AR1'])
+        x2 = x2.sel(cell_id=x2.cell_diagnosis!='')
+        x2 = x2.todense()
+        x2 = xa.merge([
+            x2.counts.assign_coords(
+                feature_id=['counts_'+k for k in x2.feature_id.data]
+            ).to_dataset(dim='feature_id'),
+            x2.rpk.assign_coords(
+                feature_id=['rpk_'+k for k in x2.feature_id.data]
+            ).to_dataset(dim='feature_id'),
+            x2.drop_dims(['feature_id', 'umap_dim']),
+            x2.umap.to_dataset('umap_dim'),
+        ])
+        x2 = x2.to_dataframe()
+        x2['C5AR1>0'] = np.where(x2.counts_C5AR1>0, 'C5AR1>0', 'C5AR1=0')
+        x2['cell_integrated_snn_res.0.3'] = x2['cell_integrated_snn_res.0.3'].astype('category')
+        x2['rpk_C5AR1_q3'] = np.where(x2.rpk_C5AR1==0, np.nan, x2.rpk_C5AR1)
+        x2['rpk_C5AR1_q3'] = pd.qcut(x2.rpk_C5AR1_q3, q=3)
+        x1 = x2['rpk_C5AR1_q3'].cat.add_categories(pd.Interval(0,0))
+        x1 = x1.cat.reorder_categories(np.roll(np.array(x1.dtype.categories), 1))
+        x1 = x1.fillna(pd.Interval(0,0))
+        x2['rpk_C5AR1_q3'] = x1
+
+        x2clust = x2.groupby('cell_integrated_snn_res.0.3')[['UMAP_1', 'UMAP_2']].\
+            mean().reset_index()
+
+        return (x2, x2clust)
+
+analysis = _analysis()
 
 # %%
-class _analysis:
+class _c5ar1_analysis:
     @property
     def data(self):
+        x = analysis.data
         x2 = x.sel(feature_id='C5AR1').drop('feature_id').todense()
         x2 = x2.sel(cell_id=x2.cell_diagnosis!='')
         x2['rpk'] = np.log1p(x2.rpk)
@@ -87,32 +125,31 @@ class _analysis:
         x3 = (x3.s-x3.m**2)**0.5
 
         x5 = self.data[['c5ar1', 'cell_diagnosis']].sel(coef='rpk').drop('coef')
-        x5 = x5.groupby('cell_diagnosis').apply(lambda x: x.c5ar1.var(dim='cell_id'))
+        x5 = x5.groupby('cell_diagnosis').apply(lambda x: x.c5ar1.std(dim='cell_id'))
 
         x2 = self.model.X
         x2 = x2.sel(coef='rpk')*x5/x3
         return x2
 
-    @property
+    @compose(property, lazy)
+    def sigs(self):
+        from decoi_atlas.sigs import sigs
+        return sigs.all1.rename('s')
+
+    @compose(property, lazy, XArrayCache())
     def t(self):
-        x4 = xa.merge([
+        import sparse        
+        from decoi_atlas.sigs.entrez import symbol_entrez
+
+        x7 = xa.merge([
             self.model.drop('X'),
             self.model1.rename('X')
         ])
-        x4 = xa.DataArray(
-            np.where(x4.q<0.1, x4.X, 0),
-            [x4.cell_diagnosis, x4.feature_id]
+        x7 = xa.DataArray(
+            np.where(x7.q<0.1, x7.X, 0),
+            [x7.cell_diagnosis, x7.feature_id]
         )
-        return x4
 
-    @compose(property, lazy, XArrayCache())
-    def gsea(self):
-        import sparse
-        from decoi_atlas.sigs import sigs
-        from decoi_atlas.sigs.fit import fit_gsea
-        from decoi_atlas.sigs.entrez import symbol_entrez
-
-        x7 = self.t
         x6 = x7.feature_id.to_series()
         x6 = symbol_entrez(x6)
         x6 = x6.rename(
@@ -126,20 +163,25 @@ class _analysis:
             join='inner'
         )
         x7 = x7/x6.sum(dim='feature_id').todense()
-        x7 = xa.merge([x7.rename('t'), sigs.all1.rename('s')], join='inner')
-        x7.t.data = sparse.COO(x7.t.data)
-        x8 = fit_gsea(x7.t, x7.s, 1e5)
+        x7.data = sparse.COO(x7.data)
 
         x9 = x6.to_series_sparse().reset_index()
         x9 = x9.groupby('gene').feature_id.apply(lambda x: ','.join(x)).rename('symbol')
         x9 = x9.to_xarray().rename('feature_ids')
 
-        x8 = xa.merge([x8, x7.t, x6, x9], join='inner')
+        x7 = xa.merge([x7.rename('t'), x9], join='inner')
 
+        return x7
+
+    @compose(property, lazy, XArrayCache())
+    def gsea(self):
+        from decoi_atlas.sigs.fit import fit_gsea
+        x8 = xa.merge([self.t, self.sigs], join='inner')
+        x8 = fit_gsea(x8.t, x8.s, 1e5)
         return x8
 
 # %%
-class _analysis1(_analysis):
+class _analysis1(_c5ar1_analysis):
     storage = config.cache/'playground4'/'analysis1'
 
     @compose(property, lazy)
@@ -150,10 +192,8 @@ class _analysis1(_analysis):
 
 analysis1 = _analysis1()
 
-self = analysis1
-
 # %%
-class _analysis2(_analysis):
+class _analysis2(_c5ar1_analysis):
     storage = config.cache/'playground4'/'analysis2'
 
     @compose(property, lazy)
@@ -164,6 +204,13 @@ class _analysis2(_analysis):
         return x2
 
 analysis2 = _analysis2()
+
+# %%
+class _analysis3(_c5ar1_analysis):
+    storage = config.cache/'playground4'/'analysis3'
+    pass
+
+analysis3 = _analysis3()
 
 # %%
 def plot_table(x3):
@@ -195,36 +242,8 @@ def plot_table(x3):
             )
     )
 
-
 # %%
-x2 = x.sel(feature_id=['C5AR1'])
-x2 = x2.sel(cell_id=x2.cell_diagnosis!='')
-x2 = x2.todense()
-x2 = xa.merge([
-    x2.counts.assign_coords(
-        feature_id=['counts_'+k for k in x2.feature_id.data]
-    ).to_dataset(dim='feature_id'),
-    x2.rpk.assign_coords(
-        feature_id=['rpk_'+k for k in x2.feature_id.data]
-    ).to_dataset(dim='feature_id'),
-    x2.drop_dims(['feature_id', 'umap_dim']),
-    x2.umap.to_dataset('umap_dim'),
-])
-x2 = x2.to_dataframe()
-x2['C5AR1>0'] = np.where(x2.counts_C5AR1>0, 'C5AR1>0', 'C5AR1=0')
-x2['cell_integrated_snn_res.0.3'] = x2['cell_integrated_snn_res.0.3'].astype('category')
-x2['rpk_C5AR1_q3'] = np.where(x2.rpk_C5AR1==0, np.nan, x2.rpk_C5AR1)
-x2['rpk_C5AR1_q3'] = pd.qcut(x2.rpk_C5AR1_q3, q=3)
-x1 = x2['rpk_C5AR1_q3'].cat.add_categories(pd.Interval(0,0))
-x1 = x1.cat.reorder_categories(np.roll(np.array(x1.dtype.categories), 1))
-x1 = x1.fillna(pd.Interval(0,0))
-x2['rpk_C5AR1_q3'] = x1
-
-x2clust = x2.groupby('cell_integrated_snn_res.0.3')[['UMAP_1', 'UMAP_2']].\
-    mean().reset_index()
-
-
-# %%
+x2, x2clust = analysis.data1
 print(
     ggplot(x2)+
         aes('UMAP_1', 'UMAP_2')+
@@ -274,6 +293,107 @@ print(
 )
 
 # %%
+x3 = analysis3.data
+x3 = x3.sel(feature_id=['IFITM2']).todense()
+x3 = x3.sel(coef='rpk')
+x3 = x3.to_dataframe().reset_index()
+x3['c5ar1_nz'] = np.where(x3.c5ar1>0, 'C5AR1>0', 'C5AR1=0')
+x3['c5ar1_nz'] = x3['c5ar1_nz'].astype('category')
+
+x4 = x3.copy()
+x4 = x4.merge(
+    x4.groupby('cell_diagnosis').\
+        apply(lambda x: x[['c5ar1', 'rpk']].\
+            corr().iloc[0,1]
+        ).\
+        rename('R').reset_index()
+)
+x4['R'] = 'R: ' + x4['R'].round(2).astype(str)
+print(
+    ggplot(x4)+aes('c5ar1', 'rpk')+
+        geom_point(aes(color='cell_diagnosis'))+
+        geom_smooth(method='lm')+
+        facet_grid('.~cell_diagnosis+R')+
+        labs(x='C5AR1', y='IFITM2')+        
+        theme(
+            figure_size=(9, 3),
+            legend_position='none'
+        ) 
+)
+
+x4 = x3.copy()
+x4['c5ar1_nz1'] = np.where(x4.c5ar1>0, 1, 0)
+x4 = x4.merge(
+    x4.groupby('cell_diagnosis').\
+        apply(lambda x: x[['c5ar1_nz1', 'rpk']].\
+            corr().iloc[0,1]
+        ).rename('R').reset_index()
+)
+x4['R'] = 'R: ' + x4['R'].round(2).astype(str)
+print(
+    ggplot(x4)+aes('c5ar1_nz', 'rpk')+
+        geom_boxplot(aes(color='cell_diagnosis'))+
+        geom_smooth(method='lm')+
+        facet_grid('.~cell_diagnosis+R')+
+        labs(x='C5AR1>0', y='IFITM2')+
+        theme(
+            figure_size=(9, 3),
+            legend_position='none'
+        ) 
+)
+
+x4 = x3.copy()
+x4 = x4[x4.c5ar1>0]
+x4 = x4.merge(
+    x4.groupby('cell_diagnosis').\
+        apply(lambda x: x[['c5ar1', 'rpk']].\
+            corr().iloc[0,1]
+        ).rename('R').reset_index()
+)
+x4['R'] = 'R: ' + x4['R'].round(2).astype(str)
+print(
+    ggplot(x4)+aes('c5ar1', 'rpk')+
+        geom_point(aes(color='cell_diagnosis'))+
+        geom_smooth(method='lm')+
+        facet_grid('.~cell_diagnosis+R')+
+        labs(x='C5AR1', y='IFITM2')+
+        theme(
+            figure_size=(9, 3),
+            legend_position='none'
+        ) 
+)
+
+# %%
+x3 = xa.merge([
+    analysis1.sigs.sel(sig='HALLMARK_INTERFERON_GAMMA_RESPONSE'),
+    analysis1.gsea.sel(sig='HALLMARK_INTERFERON_GAMMA_RESPONSE'),
+    analysis1.t
+], join='inner').todense()
+x3 = x3.to_dataframe().reset_index()
+x3['s'] = np.where(x3.s==1, 'yes', 'no')
+x3['leadingEdge'] = np.where(x3.leadingEdge==1, 'yes', 'no')
+x3['NES'] = 'NES: ' + x3.NES.round(2).astype(str)
+
+print(
+    ggplot(x3)+
+        aes('s', 'np.clip(t, -0.5, 0.5)')+
+        geom_boxplot()+
+        geom_point(aes(color='leadingEdge'))+
+        geom_label(
+            x3[x3.feature_ids=='IFITM2'], 
+            aes(label='feature_ids', color='leadingEdge')
+        )+
+        facet_grid('.~cell_diagnosis+NES')+
+        labs(
+            x='HALLMARK_INTERFERON_GAMMA_RESPONSE',
+            y='t'
+        )+
+        theme(
+            figure_size=(10, 3)
+        )
+)
+
+# %%
 x9 = analysis1.gsea
 x9 = x9[['sig','ES', 'NES', 'padj','size']]
 x9 = x9.to_dataframe().reset_index()
@@ -282,12 +402,18 @@ x9 = x9[x9['padj'].min(axis=1)<0.05]
 x9 = x9[x9['size'].min(axis=1)>5]
 x9 = x9['NES']
 x9 = x9.add(-x9['control'], axis=0).assign(control=x9['control'])
+x9 = x9.reset_index()
+x9
 
 # %%
 print(
     ggplot(x9)+
         aes('COVID-19, severe', 'COVID-19, mild', color='control')+
         geom_point()+
+        geom_label(
+            x9[x9.sig=='HALLMARK_INTERFERON_GAMMA_RESPONSE'],
+            aes(label='sig')
+        )+
         scale_color_gradient2(
             low='blue', mid='white', high='red',
             midpoint=0
@@ -295,13 +421,35 @@ print(
 )
 
 # %%
-x10 = analysis1.gsea
-x10 = x10.sel(sig='HALLMARK_INTERFERON_GAMMA_RESPONSE')
-x10 = x10.sel(cell_diagnosis='COVID-19, severe')
-x10 = x10.sel(gene=(x10.leadingEdge==1).todense())
-x10 = x10.drop_dims('feature_id')
-x10 = x10[['t', 'feature_ids']].todense()
-x10 = x10.to_dataframe().reset_index()
+x3 = xa.merge([
+    analysis2.sigs.sel(sig='HALLMARK_INTERFERON_GAMMA_RESPONSE'),
+    analysis2.gsea.sel(sig='HALLMARK_INTERFERON_GAMMA_RESPONSE'),
+    analysis2.t
+], join='inner').todense()
+x3 = x3.to_dataframe().reset_index()
+x3['s'] = np.where(x3.s==1, 'yes', 'no')
+x3['leadingEdge'] = np.where(x3.leadingEdge==1, 'yes', 'no')
+x3['NES'] = 'NES: ' + x3.NES.round(2).astype(str)
+
+print(
+    ggplot(x3)+
+        aes('s', 'np.clip(t, -0.5, 0.5)')+
+        geom_boxplot()+
+        geom_point(aes(color='leadingEdge'))+
+        geom_label(
+            x3[x3.feature_ids=='IFITM2'], 
+            aes(label='feature_ids', color='leadingEdge')
+        )+
+        facet_grid('.~cell_diagnosis+NES')+
+        labs(
+            x='HALLMARK_INTERFERON_GAMMA_RESPONSE',
+            y='t'
+        )+
+        theme(
+            figure_size=(10, 3)
+        )
+)
+
 
 # %%
 x9 = analysis2.gsea
@@ -312,26 +460,22 @@ x9 = x9[x9['padj'].min(axis=1)<0.05]
 x9 = x9[x9['size'].min(axis=1)>5]
 x9 = x9['NES']
 x9 = x9.add(-x9['control'], axis=0).assign(control=x9['control'])
+x9 = x9.reset_index()
+x9
 
 # %%
 print(
     ggplot(x9)+
         aes('COVID-19, severe', 'COVID-19, mild', color='control')+
         geom_point()+
+        geom_label(
+            x9[x9.sig=='HALLMARK_INTERFERON_GAMMA_RESPONSE'],
+            aes(label='sig')
+        )+
         scale_color_gradient2(
             low='blue', mid='white', high='red',
             midpoint=0
         )
 )
-
-
-# %%
-x10 = analysis2.gsea
-x10 = x10.sel(sig='HALLMARK_MYC_TARGETS_V1')
-x10 = x10.sel(cell_diagnosis='COVID-19, mild')
-x10 = x10.sel(gene=(x10.leadingEdge==1).todense())
-x10 = x10.drop_dims('feature_id')
-x10 = x10[['t', 'feature_ids']].todense()
-x10 = x10.to_dataframe().reset_index()
 
 # %%
