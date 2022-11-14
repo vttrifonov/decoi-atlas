73a0982056ee67b5b26014281f94168bdb6e04d2
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-11-14 18:19:48 +0000

    playground

--- playground7.py
+++ playground7.py
@@ -17,76 +17,6 @@ from ._helpers import config, plot_table, quantile
 
 storage = config.cache/'playground7'
 
-# %%
-x1 = data.c1_pbmc
-x1 = x1.rename(cell_group_per_sample='cell_diagnosis')
-x1 = x1[['cell_blueprint.labels', 'cell_diagnosis']]
-x1 = x1.to_dataframe()
-x1 = sm.stats.Table.from_data(x1)
-print(plot_table(x1))
-
-# %%
-x1 = data.c1_pbmc
-x1 = x1.rename(cell_group_per_sample='cell_diagnosis')
-x1 = x1[['cell_blueprint.labels', 'cell_diagnosis', 'cell_donor']]
-x1 = x1.to_dataframe()
-x2 = x1.groupby(['cell_blueprint.labels', 'cell_diagnosis', 'cell_donor']).size()
-x3 = x1.groupby(['cell_diagnosis', 'cell_donor']).size()
-x4 = pd.merge(
-    x2.rename('n').reset_index(), 
-    x3.rename('m').reset_index()
-)
-x4['freq'] = x4.n/x4.m
-x4 = x4[x4['cell_blueprint.labels']=='NK cells']
-x4 = x4[x4['cell_diagnosis']!='']
-print(
-    ggplot(x4)+aes('cell_diagnosis', '100*freq')+
-        geom_violin(aes(fill='cell_diagnosis'))+
-        geom_boxplot(aes(fill='cell_diagnosis'), width=0.2)+
-        geom_jitter(width=0.1)+
-        labs(x='', y='NK cells frequency (%)')+
-        theme(
-            figure_size=(4, 3),
-            legend_position='none'
-        ) 
-)
-
-# %%
-x1 = data.c2_wb_pbmc
-x1 = x1.sel(cell_id=x1.cell_purification=='FreshPBMC')
-x1 = x1.sel(cell_id=x1.cell_diagnosis!='')
-x1 = x1[['cell_blueprint.labels', 'cell_diagnosis']]
-x1 = x1.to_dataframe()
-x1 = sm.stats.Table.from_data(x1)
-print(plot_table(x1))
-
-# %%
-x1 = data.c2_wb_pbmc
-x1 = x1.sel(cell_id=x1.cell_purification=='FreshPBMC')
-x1 = x1.sel(cell_id=x1.cell_diagnosis!='')
-x1 = x1[['cell_blueprint.labels', 'cell_diagnosis', 'cell_donor']]
-x1 = x1.to_dataframe()
-x2 = x1.groupby(['cell_blueprint.labels', 'cell_diagnosis', 'cell_donor']).size()
-x3 = x1.groupby(['cell_diagnosis', 'cell_donor']).size()
-x4 = pd.merge(
-    x2.rename('n').reset_index(), 
-    x3.rename('m').reset_index()
-)
-x4['freq'] = x4.n/x4.m
-x4 = x4[x4['cell_blueprint.labels']=='NK cells']
-x4 = x4[x4['cell_diagnosis']!='']
-print(
-    ggplot(x4)+aes('cell_diagnosis', '100*freq')+
-        geom_violin(aes(fill='cell_diagnosis'))+
-        geom_boxplot(aes(fill='cell_diagnosis'), width=0.2)+
-        geom_jitter(width=0.1)+
-        labs(x='', y='NK cells frequency (%)')+
-        theme(
-            figure_size=(5, 4),
-            legend_position='none'
-        )
-)
-
 #%% 
 from types import SimpleNamespace
 
@@ -183,6 +113,76 @@ class _analysis1:
         x5['sig'] = x5.sig.str.replace('^[^_]*_', '', regex=True)
         x5 = x5.sort_values('score', ascending=False)
         return x5
+
+# %%
+x1 = data.c1_pbmc
+x1 = x1.rename(cell_group_per_sample='cell_diagnosis')
+x1 = x1[['cell_blueprint.labels', 'cell_diagnosis']]
+x1 = x1.to_dataframe()
+x1 = sm.stats.Table.from_data(x1)
+print(plot_table(x1))
+
+# %%
+x1 = data.c1_pbmc
+x1 = x1.rename(cell_group_per_sample='cell_diagnosis')
+x1 = x1[['cell_blueprint.labels', 'cell_diagnosis', 'cell_donor']]
+x1 = x1.to_dataframe()
+x2 = x1.groupby(['cell_blueprint.labels', 'cell_diagnosis', 'cell_donor']).size()
+x3 = x1.groupby(['cell_diagnosis', 'cell_donor']).size()
+x4 = pd.merge(
+    x2.rename('n').reset_index(), 
+    x3.rename('m').reset_index()
+)
+x4['freq'] = x4.n/x4.m
+x4 = x4[x4['cell_blueprint.labels']=='NK cells']
+x4 = x4[x4['cell_diagnosis']!='']
+print(
+    ggplot(x4)+aes('cell_diagnosis', '100*freq')+
+        geom_violin(aes(fill='cell_diagnosis'))+
+        geom_boxplot(aes(fill='cell_diagnosis'), width=0.2)+
+        geom_jitter(width=0.1)+
+        labs(x='', y='NK cells frequency (%)')+
+        theme(
+            figure_size=(4, 3),
+            legend_position='none'
+        ) 
+)
+
+# %%
+x1 = data.c2_wb_pbmc
+x1 = x1.sel(cell_id=x1.cell_purification=='FreshPBMC')
+x1 = x1.sel(cell_id=x1.cell_diagnosis!='')
+x1 = x1[['cell_blueprint.labels', 'cell_diagnosis']]
+x1 = x1.to_dataframe()
+x1 = sm.stats.Table.from_data(x1)
+print(plot_table(x1))
+
+# %%
+x1 = data.c2_wb_pbmc
+x1 = x1.sel(cell_id=x1.cell_purification=='FreshPBMC')
+x1 = x1.sel(cell_id=x1.cell_diagnosis!='')
+x1 = x1[['cell_blueprint.labels', 'cell_diagnosis', 'cell_donor']]
+x1 = x1.to_dataframe()
+x2 = x1.groupby(['cell_blueprint.labels', 'cell_diagnosis', 'cell_donor']).size()
+x3 = x1.groupby(['cell_diagnosis', 'cell_donor']).size()
+x4 = pd.merge(
+    x2.rename('n').reset_index(), 
+    x3.rename('m').reset_index()
+)
+x4['freq'] = x4.n/x4.m
+x4 = x4[x4['cell_blueprint.labels']=='NK cells']
+x4 = x4[x4['cell_diagnosis']!='']
+print(
+    ggplot(x4)+aes('cell_diagnosis', '100*freq')+
+        geom_violin(aes(fill='cell_diagnosis'))+
+        geom_boxplot(aes(fill='cell_diagnosis'), width=0.2)+
+        geom_jitter(width=0.1)+
+        labs(x='', y='NK cells frequency (%)')+
+        theme(
+            figure_size=(5, 4),
+            legend_position='none'
+        )
+)
 
 # %%
 class _c1_pbmc_analysis1(_analysis1):
