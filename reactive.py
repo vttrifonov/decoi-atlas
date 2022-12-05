111661556b74d2566b4aaa4174e85c44dffa349b
Trifonov, Vladimir [JANUS] <VTrifono@its.jnj.com>
2022-12-05 13:58:44 -0800

    reactive

--- reactive.py
+++ reactive.py
@@ -1,16 +1,4 @@
 # %%
-import ipywidgets as w
-import traitlets as t
-import IPython.display as ipd
-from types import SimpleNamespace as namespace
-
-# %%
-class ValueProvider:
-    pass
-
-class ValueConsumer:
-    pass
-
 class Observable:
     def __init__(self):
         self._observers = set()
@@ -25,23 +13,24 @@ class Observable:
     def unobserve(self, o):
         self._observers.remove(o)
 
-def _is_observable_value_provider(x):
-    return isinstance(x, Observable) and isinstance(x, ValueProvider)
+class ValueProvider:
+    def __init__(self):
+        self._dirty = Observable()
 
 def observe(*args):
     def wrap(f):
         def observer():
             f(*[a() for a in args])
         for a in args:
-            a.observe(observer)
+            a._dirty.observe(observer)
         return f
     return wrap
 
 class _None:
     pass
 _None = _None()
-
-class Value(Observable, ValueProvider, ValueConsumer):
+        
+class Value(ValueProvider):
     def __init__(self, value=_None):
         super().__init__()
         self._value = value
@@ -53,45 +42,50 @@ class Value(Observable, ValueProvider, ValueConsumer):
                 raise(ValueError('value is not initialized'))
             return value
         self._value = value
-        self._notify()
+        self._dirty._notify()
 
-class Reactive(Observable, ValueProvider):
+class Reactive(ValueProvider):
     def __init__(self, args, f):
         super().__init__()
-        self._dirty = True
+        self._is_dirty = True
         self._value = _None
 
         for a in args:
-            a.observe(self._notify)
+            a._dirty.observe(self._dirty_notify)
         self._update = lambda: f(*[a() for a in args])
 
-    def _notify(self):
-        self._dirty = True
-        super()._notify()
+    def _dirty_notify(self):
+        self._is_dirty = True
+        self._dirty._notify()
 
     def __call__(self):
-        if self._dirty:
+        if self._is_dirty:
             self._value = self._update()
-            self._dirty = False
+            self._is_dirty = False
         return self._value
 
-class _reactive_trait(Observable, ValueProvider):
+class _reactive_trait(ValueProvider):
     def __init__(self, trait):
-        self.trait = trait
-        trait[0].observe(lambda _: self._notify(), trait[1])
+        super().__init__()
+        self._trait = trait
+        trait[0].observe(lambda _: self._dirty._notify(), trait[1])
 
     def __call__(self):
-        return getattr(*self.trait)
+        return getattr(*self._trait)
 
 def reactive(*args):
     args1 = []
     for a in args:
-        if not _is_observable_value_provider(a):
+        if not isinstance(a, ValueProvider):
             if not isinstance(a, tuple):
                 a = a, 'value'
             a = _reactive_trait(a)
         args1.append(a)                        
     return lambda f: Reactive(args1, f)
+
+# %%
+import ipywidgets as w
+import IPython.display as ipd
 
 def _display(out, x, clear_output=True, wait=True):
     with out:
@@ -119,100 +113,65 @@ class HBox(_named_children, w.HBox):
         _named_children.__init__(self, children)
 
 # %%
-ui = VBox(dict(
-    i = w.IntText(2),
-    o1 = w.Output(),
-    o2 = w.Output()
-))
+if False:
+    from types import SimpleNamespace as namespace
+    import traitlets as t
 
-j = Value(2)
+    class Value(t.HasTraits):
+        value = t.Any()
 
-@reactive(ui.i, j)
-def s1(i, j):
-    print('ip1')
-    return i+j
+        def update(self, v):
+            self.value = v
 
-@reactive(ui.i)
-def s2(i):
-    print('ip2')
-    return i+2
+    def observe(*args):
+        def wrap(f):
+            def observer():
+                f(*[a.value for a in args])
+            for a in args:
+                a.observe(lambda _: observer(), 'value')    
 
-@reactive(ui.i)
-def s3(i):
-    print('x5')
-    return i+3
+        return wrap
 
-display(ui.o1, s1)
+    def observer(*args):
+        def wrap(f):
+            observe(*args)(f)
+            return f
+        return wrap
 
-display(ui.o2, s2)
+    def reactive(*args):
+        def wrap(f, v = None):
+            if v is None:
+                v = Value()        
+            observe(*args)(lambda *a: v.update(f(*a)))
+            v.update(f(*[a.value for a in args]))
+            return v
+        return wrap
 
-ui
+    def react(*args):
+        return lambda f: reactive(*args)(f)
 
-# %%
-s3()
+    class Output(w.Output):
+        def display(self, x, clear_output=True, wait=True):
+            with self:
+                if clear_output:
+                    ipd.clear_output(wait=wait)
+                ipd.display(x)
 
-# %%
-j(5)
-
-
-# %%
-class Value(t.HasTraits):
-    value = t.Any()
-
-    def update(self, v):
-        self.value = v
-
-def observe(*args):
-    def wrap(f):
-        def observer():
-            f(*[a.value for a in args])
-        for a in args:
-            a.observe(lambda _: observer(), 'value')    
-
-    return wrap
-
-def observer(*args):
-    def wrap(f):
-        observe(*args)(f)
-        return f
-    return wrap
-
-def reactive(*args):
-    def wrap(f, v = None):
-        if v is None:
-            v = Value()        
-        observe(*args)(lambda *a: v.update(f(*a)))
-        v.update(f(*[a.value for a in args]))
-        return v
-    return wrap
-
-def react(*args):
-    return lambda f: reactive(*args)(f)
-
-class Output(w.Output):
-    def display(self, x, clear_output=True, wait=True):
-        with self:
-            if clear_output:
-                ipd.clear_output(wait=wait)
-            ipd.display(x)
-
-    def react(self, x, clear_output=True, wait=True):
-        observe(x)(lambda x: self.display(x, clear_output, wait))
-        self.on_displayed(lambda *_: self.display(x.value, clear_output, wait))
+        def react(self, x, clear_output=True, wait=True):
+            observe(x)(lambda x: self.display(x, clear_output, wait))
+            self.on_displayed(lambda *_: self.display(x.value, clear_output, wait))
 
 
-class VBox(w.VBox):
-    def __init__(self, children, **kwargs):
-        w.VBox.__init__(self, list(children.values()), **kwargs)
-        self.c = namespace(**children)
+    class VBox(w.VBox):
+        def __init__(self, children, **kwargs):
+            w.VBox.__init__(self, list(children.values()), **kwargs)
+            self.c = namespace(**children)
 
 
-class HBox(w.HBox):
-    def __init__(self, children, **kwargs):
-        w.HBox.__init__(self, list(children.values()), **kwargs)
-        self.c = namespace(**children)
-
-# %%
+    class HBox(w.HBox):
+        def __init__(self, children, **kwargs):
+            w.HBox.__init__(self, list(children.values()), **kwargs)
+            self.c = namespace(**children)
 
 # %%
 if __name__ == '__main__':
