7533ba2c74c1b61c5f554b4cd1f99dfb857650d0
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-12-06 00:55:56 +0000

    reactive

--- reactive.py
+++ reactive.py
@@ -17,10 +17,30 @@ class ValueProvider:
     def __init__(self):
         self._dirty = Observable()
 
+class _reactive_trait(ValueProvider):
+    def __init__(self, trait):
+        super().__init__()
+        self._trait = trait
+        trait[0].observe(lambda _: self._dirty._notify(), trait[1])
+
+    def __call__(self):
+        return getattr(*self._trait)
+
+def _fix_args(args):
+    args1 = []
+    for a in args:
+        if not isinstance(a, ValueProvider):
+            if not isinstance(a, tuple):
+                a = a, 'value'
+            a = _reactive_trait(a)
+        args1.append(a)
+    return args1
+
 def observe(*args):
+    args = _fix_args(args)
     def wrap(f):
         def observer():
-            f(*[a() for a in args])
+            f(*[a() for a in args])        
         for a in args:
             a._dirty.observe(observer)
         return f
@@ -64,30 +84,16 @@ class Reactive(ValueProvider):
             self._is_dirty = False
         return self._value
 
-class _reactive_trait(ValueProvider):
-    def __init__(self, trait):
-        super().__init__()
-        self._trait = trait
-        trait[0].observe(lambda _: self._dirty._notify(), trait[1])
-
-    def __call__(self):
-        return getattr(*self._trait)
-
-def reactive(*args):
-    args1 = []
-    for a in args:
-        if not isinstance(a, ValueProvider):
-            if not isinstance(a, tuple):
-                a = a, 'value'
-            a = _reactive_trait(a)
-        args1.append(a)                        
-    return lambda f: Reactive(args1, f)
+def reactive(*args):                    
+    return lambda f: Reactive(_fix_args(args), f)
 
 # %%
 import ipywidgets as w
 import IPython.display as ipd
 
 def _display(out, x, clear_output, wait):
+    if x is None:
+        return
     def _display():
         if clear_output:
             ipd.clear_output(wait=wait)
