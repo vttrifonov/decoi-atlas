87100b1ef79a0f7987373a5f462e6558bd3a895c
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-12-10 18:34:48 +0000

    deg

--- deg/__init__.py
+++ deg/__init__.py
@@ -17,21 +17,29 @@ from .._helpers import config
 
 # %%
 def groupby(x1, group_id='group_id', x3 = None, f = None):
+    if group_id is None:
+        i = 0
+        while 'dim_'+str(i) in x1:
+            i = i + 1
+        group_id = 'dim_'+str(i)
+    
+    _group_id = '_'+group_id
     x1 = x1.to_dataframe()
-    x2 = x1.drop_duplicates().\
-        assign(**{group_id: lambda x: range(x.shape[0])})
-    x1 = x1.reset_index().merge(x2)[[group_id]+x1.index.names].\
+    x2 = x1.drop_duplicates().copy()
+    x2[group_id] = range(x2.shape[0])
+    x1 = x1.reset_index().\
+        merge(x2)[[group_id]+x1.index.names].\
         set_index(x1.index.names).to_xarray().\
-        rename({group_id: '_sample_group_id'})
+        rename({group_id: _group_id})
     x2 = x2.set_index(group_id).to_xarray()
     x1 = xa.merge([x1, x2], join='inner')
     if f is None:
         return x1
 
-    x3 = x3.groupby(x1._sample_group_id).apply(f)
+    x3 = x3.groupby(x1[_group_id]).apply(f)
     x3 = xa.merge([
         x1,
-        x3.rename(_sample_group_id=group_id)
+        x3.rename({_group_id: group_id})
     ], join='inner')
     return x3
 
@@ -85,18 +93,24 @@ class _analysis:
     @compose(property, lazy)
     def data2(self):
         x1 = self.data1.copy()
-        x1 = x1.drop('_sample_group_id')
+        x1 = x1.drop('_group_id1')
 
-        x2 = x1.drop_dims(['cell_id', 'feature_id']).drop('blueprint.labels')
-        x2 = groupby(x2.drop('n'), 'group_id2', x2.n, lambda x: x.sum(dim='group_id1'))
-        x1['freq'] = x1.n/x2.n[x2._sample_group_id].drop('group_id2')
+        x2 = x1.drop_dims(['cell_id', 'feature_id'])\
+            .drop('blueprint.labels')
+        x2 = groupby(
+            x2.drop('n'), 'group_id2', 
+            x2.n, lambda x: x.sum(dim='group_id1')
+        )
+        x2 = x2.n[x2._group_id2].drop('group_id2')
+        x1['freq'] = x1.n/x2
 
-        x3 = x1.drop_dims('cell_id').drop(['experiment', 'disease_stage', 'n'])
+        x3 = x1.drop_dims('cell_id').\
+            drop(['experiment', 'disease_stage', 'n'])
         x3 = groupby(
             x3.drop_dims('feature_id').drop('freq'), 
             'group_id2',
             x3[['counts', 'freq']],
-            lambda x: x.mean(dim='group_id1'),
+            lambda x: x.mean(dim='group_id1')
         )
 
         return x3
@@ -111,43 +125,56 @@ class _analysis:
         model1 = lambda d, s: fit(R.fit, d, s, '~group_per_sample')
 
         x1 = self.data2.copy()
+        x1 = x1.drop_dims('group_id1')
         x1['counts'] = x1.counts.transpose('feature_id', 'group_id2')
-        x1 = x1.sel(group_id2=x1['blueprint.labels']!='Macrophages')
-        x8 = [['control', 'mild'], ['control', 'severe'], ['mild', 'severe']]
+        x8 = [['control', 'mild'], ['control', 'severe'], ['mild', 'severe']]        
 
-        def fit1(x9):
-            x9 = x9.sel(group_id3=x9._sample_group_id.data[0])
+        def fit1(x9, i):
+            x9 = x9.sel(group_id3=x9._group_id3.data[0])
             print(
                 x9['purification'].item(),
-                x9['blueprint.labels'].item()
+                x9['blueprint.labels'].item(),
             )
-            x9 = x9.drop('group_id3')
             x11 = list()
-            for x7 in x8:  
+            for x7 in x8:
                 print(x7)
                 x4 = x9.sel(group_id2=x9.group_per_sample.isin(x7))                
                 x6 = x4.group_per_sample.to_dataframe()
+                if x6.shape[0]==0:
+                    continue
                 x12 = x6.groupby('group_per_sample').size()
                 if x12.shape[0]<len(x7) or any(x12==1):
                     continue
                 x6['group_per_sample'] = x6.group_per_sample.astype(pd.CategoricalDtype(x7))
                 x10 = model1(x4.counts, x6).drop_dims('group_id2')
-                x10 = x10.expand_dims(source=['model1_' + '_'.join(x7)])
+                x10['var'] = 'var', ['control', 'case']
+                x10['group_id4'] = 'group_id4', [i]
+                i = i + 1
+                x10['control'] = 'group_id4', [x7[0]]
+                x10['case'] = 'group_id4', [x7[1]]
+                x10['source'] = 'group_id4', ['model1_' + '_'.join(x7)]
                 x11.append(x10)
             if len(x11)==0:
-                return xa.Dataset()
-            x11 = xa.concat(x11, dim='source')
-            return x11
+                return None, i
+            x11 = xa.concat(x11, dim='group_id4')
+            x9 = x9[['purification', 'blueprint.labels', 'group_id3']].\
+                reset_coords().\
+                expand_dims(group_id4=x11.group_id4.data)
+            x11 = xa.merge([x11, x9])
+            return x11, i
 
-        #x1 = x1.sel(group_id2=x1['blueprint.labels']=='NK cells')
-        #x8 = [['control', 'mild']]
-
-        x3 = x1.drop_dims(['feature_id', 'group_id1']).drop(['freq', 'group_per_sample', 'donor'])    
+        x3 = x1.drop_dims('feature_id').drop(['freq', 'group_per_sample', 'donor'])    
         x3 = groupby(x3, 'group_id3')
         x3 = xa.merge([x1[['counts', 'group_per_sample']], x3], join='inner')
-        x4 = x3.groupby('_sample_group_id').apply(fit1)
-        x4 = x4.rename(_sample_group_id='group_id3')
-        x4 = xa.merge([x4, x3.drop(['counts', 'group_per_sample'])], join='inner')
+
+        x4 = list()
+        i = 0
+        for _, x9 in list(x3.groupby('_group_id3')):
+            x5, i = fit1(x9, i)
+            x4.append(x5)
+        x4 = [x for x in x4 if x is not None]
+        x4 = xa.concat(x4, 'group_id4')
+        x4 = xa.merge([x4, x3._group_id3], join='inner')
         
         return x4
 
@@ -155,10 +182,153 @@ analysis = _analysis()
 
 # %%
 if __name__ == '__main__':
-    self = analysis    
+    self = analysis   
+    
+    # %%
+    def cor(x1, x2, dim):
+        x3 = x1*x2
+        x4 = ~x3.isnull()
+        x4 = x4/x4
+
+        x1 = x1*x4
+        x1 = x1 - x1.mean(dim=dim)
+        x1 /= np.sqrt((x1**2).sum(dim=dim))
+        x2 = x2*x4
+        x2 = x2 - x2.mean(dim=dim)
+        x2 /= np.sqrt((x2**2).sum(dim=dim))
+        x3 = (x1*x2).sum(dim=dim)
+        x3 = x3.rename('cor')
+        return x3    
 
     # %%
-    self.model1
+    x2 = self.model1.copy()
+    x2 = x2.drop_dims('group_id2')
+    x2 = x2.rename(group_id4='group_id')
+
+    # %%
+    x2['logit_p_value'] = np.log2(x2.p/(1-x2.p))
+
+    # %%
+    x3 = x2.coef.sel(var='case')
+    x4 = cor(
+        x3,
+        x3.rename(group_id='group_id1'),
+        'feature_id'
+    )
+
+    # %%
+    from plotnine import *
+
+    x5 = x4.to_dataframe().reset_index()
+    x5 = x5[~x5.cor.isna()]
+
+    # %%
+    (
+        ggplot(x5)+aes('cor')+
+            geom_freqpoly(bins=30)
+    )
+    
+
+    # %%
+    from scipy.cluster.hierarchy import linkage, dendrogram
+    from scipy.spatial.distance import squareform
+    
+    x6 = (1-x4.data)/2
+    x6 = x6 - np.diag(np.diag(x6))
+    x6 = squareform(x6)
+    x6 = linkage(x6)
+    dendrogram(x6, labels=x4.group_id.data, color_threshold=0.17)
+    x6 = dendrogram(x6, no_plot=True, labels=x4.group_id.data, color_threshold=0.17)
+    x6 = pd.DataFrame(dict(
+        group_id=x6['ivl'],
+        ord=range(len(x6['ivl'])),            
+        color=x6['leaves_color_list']
+    ))
+
+    # %%
+    x7 = x6.sort_values('ord').group_id.to_list()
+    for x in ['group_id', 'group_id1']:
+        x5[x] = x5[x].astype(pd.CategoricalDtype(x7))
+
+    # %%
+    (
+        ggplot(x5)+
+            aes('group_id', 'group_id1', fill='cor')+
+            geom_tile()+
+            scale_fill_gradient2(
+                low='blue', mid='white', high='red',
+                midpoint=0
+            )+
+            theme(axis_text=element_text(size=6))
+    )
+
+    # %%
+    x8 = x6.merge(
+        x2[['source', 'purification', 'blueprint.labels']].\
+            to_dataframe().reset_index()
+    ).sort_values('ord')
+    x8
+
+    # %%
+    x9 = x6.set_index('group_id').color.to_xarray()
+    x9 = xa.merge([x9,  x2[['purification', 'blueprint.labels', 'source']]])
+    x9 = xa.merge([
+        x4, 
+        x9, 
+        x9.rename({k: k+'1' for k in ['group_id']+list(x9.keys())})
+    ])
+    x9 = x9.to_dataframe().reset_index()
+    x9 = x9[x9.group_id!=x9.group_id1]
+    x9['g'] = np.apply_along_axis(
+        lambda x: np.array('\n'.join(np.sort(x)), dtype=object),
+        1,
+        x9[['color', 'color1']].to_numpy()
+    )
+    (
+        ggplot(x9)+
+            aes('g', 'cor')+
+            geom_violin()+geom_point()+
+            theme(
+                axis_text_x=element_text(angle=45, hjust=1)
+            )
+    )
+
+    # %%
+    x10 = [26, 44]
+    x10 = x2.sel(var='case', group_id=x10).\
+        logit_p_value.\
+        to_dataset(dim='group_id').\
+        rename(dict(zip(x10, ['x', 'y']))).\
+        to_dataframe().reset_index()
+    
+    (
+        ggplot(x10)+
+            aes('x', 'y')+
+            geom_point()
+    )
+    
+    # %%
+    x11 = x2.sel(var='case')
+    #x11 = x11.sel(group_id=x11['blueprint.labels']=='Neutrophils')
+    #x11 = x11.sel(group_id=x11.purification=='FreshEryLysis')
+    x11 = x11.sel(group_id=x11.source=='model1_control_severe')
+    x11 = x11.to_dataframe().reset_index()
+    x11 = x11[x11.p<1e-4]
+    (
+        ggplot(x11)+
+            aes('np.clip(coef, -5, 5)', 'np.clip(-np.log10(p), 0, 5)')+
+            geom_point()
+    )
+
+    # %%
+    x12 = x2.sel(var='case')
+    #x12 = x12.sel(group_id=x12.source=='model1_control_mild')
+    x12 = x12.sel(group_id=x12['blueprint.labels']=='Neutrophils')
+    x12 = x12.to_dataframe().reset_index()
+    x12 = x12[x12.p<1e-4]
+    x12 = x12.groupby(['purification', 'blueprint.labels', 'source']).size()
+    x12 = x12.sort_values()
+    x12
 
     # %%
     from rpy2.robjects import r as R
